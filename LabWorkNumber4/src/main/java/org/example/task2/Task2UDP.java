5758ac40c90db4bc0daebcadf9dec3e77893d03e
Vlad.Zherko <vladikon19@tut.by>
2024-09-10 18:26:28 +0300

    renamed

--- LabWorkNumber4/src/main/java/org/example/task2/Task2UDP.java
+++ LabWorkNumber4/src/main/java/org/example/task2/Task2UDP.java
@@ -3,7 +3,7 @@ package org.example.task2;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
-public class Task2 {
+public class Task2UDP {
     public static void main(String[] args) throws SocketException, UnknownHostException {
         new EchoServer().start();
 
